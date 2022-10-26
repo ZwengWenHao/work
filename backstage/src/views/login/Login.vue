7a06d48ef6ee6877bb5572910df93df32a62ddc9
Zeng <1474823508@qq.com>
2022-10-27 00:36:13 +0800

    ..

--- backstage/src/views/login/Login.vue
+++ backstage/src/views/login/Login.vue
@@ -6,24 +6,40 @@ const data = reactive({
   form: {
     username: "",
     password: "",
+    userPassword: "",
   },
   checked1: false,
+  NewStatus: false,
+  loginName: "账号密码登录",
 });
-const { form, checked1 } = toRefs(data);
+const NewUser = () => {
+  data.NewStatus = true;
+  if (data.NewStatus) {
+    data.loginName = "账号密码注册";
+  } else if(!data.NewStatus) {
+    data.loginName = "账号密码注册";
+  }
+};
+const { form, checked1, NewStatus, loginName } = toRefs(data);
 </script>
 
 <template>
   <div class="login">
-    <div class="login_box">
+    <div class="login_box" v-show="!NewStatus">
       <div class="login_header">
-        <h2>在线学习平台</h2>
-        <p>拥有丰富的学习资源最好的学习平台之一</p>
+        <h1 style="font-weight: 900">在线学习平台</h1>
+        <p style="color: #409eff">拥有丰富的学习资源最好的学习平台之一</p>
+        <div class="login_title">
+          <p style="color: #409eff">{{ loginName }}</p>
+        </div>
+        <el-divider />
       </div>
       <el-form :model="form">
         <el-form-item>
           <el-input
             :prefix-icon="User"
             :clearable="true"
+            placeholder="请输入账号"
             v-model="form.username"
           />
         </el-form-item>
@@ -33,6 +49,7 @@ const { form, checked1 } = toRefs(data);
           <el-input
             :prefix-icon="Lock"
             type="password"
+            placeholder="请输入密码"
             v-model="form.password"
             show-password
           />
@@ -40,10 +57,59 @@ const { form, checked1 } = toRefs(data);
       </el-form>
       <div class="login_footer">
         <el-checkbox v-model="checked1" size="large">自动登录</el-checkbox>
-        <el-button type="text">忘记密码</el-button>
+        <span @click="NewUser">新用户注册</span>
+        <span>忘记密码</span>
       </div>
       <el-button type="primary" style="width: 100%; margin-top: 10px"
         >登录</el-button
+      >
+    </div>
+
+    <div class="login_box" v-show="NewStatus">
+      <div class="login_header">
+        <h1 style="font-weight: 900">在线学习平台</h1>
+        <p style="color: #409eff">拥有丰富的学习资源最好的学习平台之一</p>
+      </div>
+      <div class="login_title">
+        <p style="color: #409eff">{{ loginName }}</p>
+      </div>
+      <el-divider />
+      <el-form :model="form">
+        <el-form-item>
+          <el-input
+            :prefix-icon="User"
+            :clearable="true"
+            placeholder="请手机号"
+            v-model="form.username"
+          />
+        </el-form-item>
+      </el-form>
+
+      <el-form :model="form">
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+            v-model="form.password"
+            show-password
+          />
+        </el-form-item>
+      </el-form>
+
+      <el-form :model="form">
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请再次输入密码"
+            v-model="form.userPassword"
+            show-password
+          />
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" style="width: 100%; margin-top: 10px"
+        >注册</el-button
       >
     </div>
   </div>
@@ -54,6 +120,7 @@ const { form, checked1 } = toRefs(data);
   height: 100vh;
   position: relative;
   background: #f0f2f5;
+
   .login_box {
     position: absolute;
     width: 400px;
@@ -64,19 +131,38 @@ const { form, checked1 } = toRefs(data);
     bottom: 200px;
     margin: auto;
     text-align: center;
-    .login_header{
-      height: 150px;
+
+    .login_header {
     }
+    .login_title {
+      margin: auto;
+      position: relative;
+      margin-top: 20px;
+      height: 30px;
+      line-height: 30px;
+      p{
+        width: 100%;
+        text-align: center;
+      }
+    }
+
     .el-input {
       height: 40px;
     }
+
     .login_footer {
       display: flex;
       align-items: center;
       justify-content: space-between;
+
+      span {
+        color: #409eff;
+        cursor: pointer;
+      }
     }
   }
 }
+::v-deep .el-divider {
+  margin-top: 9px;
+}
 </style>
-
-
